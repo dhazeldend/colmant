00246a163ddf37e5075df82834dbe411ce3adda5
Richard Hazelden <dhazeldend@gmail.com>
2015-11-05 18:57:05 +0200

    animate banner

--- src/app/static/UI/banner/banner.js
+++ src/app/static/UI/banner/banner.js
@@ -26,6 +26,31 @@
 
         initialize : function(){
 
+            $('#myCarousel').bind('slid.bs.carousel', function(e){
+                if($("div.active").hasClass("blank")){
+
+                    // home screen
+                    $("#logo").animate({
+                        opacity: 1,
+                        zoom:1
+                    });
+
+                    $(".splash").removeClass("largeSplash");
+                    $("#btn-enter").removeClass("largeButton");
+
+                }else{
+
+                    //gallery screen
+                    $("#logo").animate({
+                        opacity: 0,
+                        zoom:1.14
+                    });
+
+                    $(".splash").addClass("largeSplash");
+                    $("#btn-enter").addClass("largeButton");
+                }
+            });
+
             $("#btn-enter").click(function(e){
 
                 $(".navbar").find("a[href = '#about']").click();
